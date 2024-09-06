ac7dd31f73e7b80b5c2d3d0201c4dc91bbc96858
2Usi <71983139+2Usi@users.noreply.github.com>
2024-09-07 00:33:01 +0300

    .

---
+++ 2d-portfolio/app/page.tsx
@@ -0,0 +1,9 @@
+import Portfolio2D from './components/terminal.tsx'
+import ClientPortfolio2D from './components/ClientPortfolio2D'
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Portfolio2D />
+  </main>
+  )
+}
