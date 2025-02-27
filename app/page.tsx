d0ff74446b0c6017b8f9015bbd6dd6c737279bc2
Cucuteanu Tudor 912 <71983139+Cucuteanu-Tudor-912@users.noreply.github.com>
2025-02-27 16:55:45 +0200

    imported tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -44,6 +44,7 @@ const TerminalIcon = () => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
+    className="text-green-500"
   >
     <polyline points="4 17 10 11 4 5"></polyline>
     <line x1="12" y1="19" x2="20" y2="19"></line>
